66544ea710547de20fd45b33366fdd52b91a3821
Nainlpz <nain.lopez.2003@gmail.com>
2025-02-20 13:08:52 +0100

    feat(Estacion): Pasar primer test del main

---
+++ src/main/java/domain/estacion/Estacion.java
@@ -0,0 +1,40 @@
+package domain.estacion;
+
+public class Estacion {
+
+    private final int id;
+    private final String direccion;
+    private final int anclajes;
+
+
+
+    public Estacion(int id, String direccion, int anclajes) {
+        this.id = id;
+        this.direccion = direccion;
+        this.anclajes = anclajes;
+    }
+
+    private int getId() {
+        return this.id;
+    }
+
+    private String getDireccion() {
+        return this.direccion;
+    }
+
+    private int getAnclajes() {
+        return this.anclajes;
+    }
+
+    public void consultarEstacion() {
+        System.out.println(this);
+    }
+
+    @Override
+    public String toString() {
+        return "id: " + this.getId() + "\n" +
+                "direccion: " + this.getDireccion() + "\n" +
+                "numero de anclajes: " + this.getAnclajes();
+    }
+}
+
