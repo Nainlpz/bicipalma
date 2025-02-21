4375975b58324f057eef9290ffab1ccea1a8de7e
Nainlpz <nain.lopez.2003@gmail.com>
2025-02-21 13:01:47 +0100

    test(BicicletaTest): Pasar test clase Bicicleta

---
+++ src/test/java/domain/bicicleta/BicicletaTest.java
@@ -0,0 +1,24 @@
+package domain.bicicleta;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class BicicletaTest {
+
+    private static Bicicleta bicicleta = null;
+
+    @Before
+    public void setup_bicicleta() {
+        bicicleta = new Bicicleta(101);
+    }
+    @Test
+    public void test_constructorBicicleta() {
+
+        int idExpected = 101;
+        int id = bicicleta.getId();
+        assertEquals(idExpected, id);
+    }
+}
